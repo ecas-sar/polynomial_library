d35a267213c55cad07fc7b12580c8794e7dae869
Eddie Cassar <cassar.eddie.l@macbook-pro.lan>
2026-04-16 22:50:24 +0100

    Freeing works also

--- test_suite.c
+++ test_suite.c
@@ -2,7 +2,7 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
-void allocate_test(void)
+void allocate_and_free_test(void)
 {
     polynomial_t p;
     int poly_degree_plus_one = 3;
@@ -14,13 +14,15 @@ void allocate_test(void)
     // Test polynomial member variables are what they should be.
     CU_ASSERT(p.coefficients != NULL);
     CU_ASSERT(p.poly_degree_plus_one == 3);
+
+    CU_ASSERT(free_poly(&p) == 0);
 }
 
 int main()
 {
     CU_initialize_registry();
     CU_pSuite suite = CU_add_suite("TestSuite", 0, 0);
-    CU_add_test(suite, "Allocation test", allocate_test);
+    CU_add_test(suite, "Allocation and freeing test", allocate_and_free_test);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0;
